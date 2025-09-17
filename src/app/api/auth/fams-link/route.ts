1898225de8f8a970a9e32c6bbc39c3dadcd911bb
khanh106 <shopbanhang106@gmail.com>
2025-09-17 19:52:37 +0700

    fix deploy_2

--- src/app/api/auth/fams-link/route.ts
+++ src/app/api/auth/fams-link/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: NextRequest) {
   try {
     const cookieName = process.env.COOKIE_NAME || "auth_token"
